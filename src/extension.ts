b41d41cd020f48ebfc9ac90287761a4b4de8fa12
Madyan <22075444+madyanalj@users.noreply.github.com>
2020-09-26 15:20:59 +0000

    refactor: separate logic into functions

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,20 @@
-import { ExtensionContext, languages, CodeLens, Range } from 'vscode';
+import { ExtensionContext, languages, CodeLens, Range, TextDocument } from 'vscode';
 import { runInNewContext } from 'vm';
-import { Node, ParameterDeclaration, printNode, Project, ts, createWrappedNode } from 'ts-morph';
+import { Node, ParameterDeclaration, printNode, Project, ts, createWrappedNode, SourceFile, TypeNode } from 'ts-morph';
 import { random } from 'faker';
 
 const { factory, getJSDocType } = ts;
 
-const generateArgument = (parameter: ParameterDeclaration) => {
+const generateArgumentByTypeNode = (type: TypeNode) => {
+  switch (true) {
+    case Node.isStringKeyword(type): return factory.createStringLiteral(random.words());
+    case Node.isNumberKeyword(type): return factory.createNumericLiteral(random.number({ min: -5, max: 5 }));
+    case Node.isBooleanKeyword(type): return random.boolean() ? factory.createTrue() : factory.createFalse();
+    default: return factory.createIdentifier('undefined');
+  }
+};
+
+const generateArgumentByParameterDeclaration = (parameter: ParameterDeclaration) => {
   const compilerType = parameter.getType().isAny()
     && getJSDocType(parameter.compilerNode)
     || parameter.getTypeNode()?.compilerNode;
@@ -16,45 +25,44 @@ const generateArgument = (parameter: ParameterDeclaration) => {
       : factory.createNumericLiteral(random.number({ min: -5, max: 5 }));
   }
 
-  const type = createWrappedNode(compilerType);
+  return generateArgumentByTypeNode(createWrappedNode(compilerType));
+};
 
-  switch (true) {
-    case Node.isStringKeyword(type): return factory.createStringLiteral(random.words());
-    case Node.isNumberKeyword(type): return factory.createNumericLiteral(random.number({ min: -5, max: 5 }));
-    case Node.isBooleanKeyword(type): return random.boolean() ? factory.createTrue() : factory.createFalse();
-    default: return factory.createIdentifier('undefined');
-  }
+const generateExpressions = (file: SourceFile) =>
+  file.getFunctions().map((func) => {
+    const args = func.getParameters().map(generateArgumentByParameterDeclaration);
+    const callExpression = factory.createCallExpression(func.getNameNodeOrThrow().compilerNode, undefined, args);
+    const sourceCode = printNode(callExpression);
+    const position = func.getEnd() + 1;
+    return { sourceCode, position };
+  });
+
+const provideCodeLenses = (document: TextDocument) => {
+  const project = new Project();
+  const file = project.createSourceFile(
+    '/tmp/vscode-instantcode/eval.ts',
+    document.getText(),
+  );
+  const compiledJs = project.emitToMemory({ targetSourceFile: file }).getFiles()[0].text;
+
+  const generatedExpressions = generateExpressions(file);
+
+  const codeLensDetails = generatedExpressions.map(({ sourceCode, position }) => {
+    const result = runInNewContext(`${compiledJs}; ${sourceCode}`);
+    return {
+      title: `${sourceCode} => ${JSON.stringify(result, null, 1)}`,
+      position: document.positionAt(position),
+    };
+  });
+
+  return codeLensDetails.map(({ title, position }) =>
+    new CodeLens(new Range(position, position), { title, command: '' }),
+  );
 };
 
 export const activate = (context: ExtensionContext) => {
-  const disposable = languages.registerCodeLensProvider(['typescript', 'javascript'], {
-    provideCodeLenses: async (document) => {
-      const project = new Project();
-      const file = project.createSourceFile(
-        '/tmp/vscode-instantcode/eval.ts',
-        document.getText(),
-      );
-      const functions = file.getFunctions();
-      const compiledJs = project.emitToMemory({ targetSourceFile: file }).getFiles()[0].text;
-
-      const functionCalls = functions.map((func) => {
-        const args = func.getParameters().map(generateArgument);
-        const callExpression = factory.createCallExpression(func.getNameNodeOrThrow().compilerNode, undefined, args);
-        const sourceCode = printNode(callExpression);
-        const result = runInNewContext(`${compiledJs}; ${sourceCode}`);
-        return { func, sourceCode, result };
-      });
-
-      const codeLensDetails = functionCalls.map(({ func, sourceCode, result }) => ({
-        title: `${sourceCode} => ${JSON.stringify(result, null, 1)}`,
-        position: document.positionAt(func.getEnd()).translate(1),
-      }));
-
-      return codeLensDetails.map(({ title, position }) =>
-        new CodeLens(new Range(position, position), { title, command: '' }),
-      );
-    },
-  });
-
-  context.subscriptions.push(disposable);
+  context.subscriptions.push(languages.registerCodeLensProvider(
+    ['typescript', 'javascript'],
+    { provideCodeLenses },
+  ));
 };
